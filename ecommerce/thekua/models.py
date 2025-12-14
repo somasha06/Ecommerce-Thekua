57e12465a3510d8a1bae9af9cdb06f1c33d1c2e2
somasha06 <somasha09@gmil.com>
2025-12-14 23:50:25 +0530

    Fix custom user model and profile creation

--- ecommerce/thekua/models.py
+++ ecommerce/thekua/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 import uuid
 from django.utils import timezone
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 # Create your models here.
 
 class User(AbstractUser):
@@ -58,3 +60,9 @@ class Profile(models.Model):
 
     def __str__(self):
         return f"{self.user.username}-profile"
+    
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
